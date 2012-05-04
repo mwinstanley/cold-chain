94384412d47a4338ca439cb13c1dc71e7f1e400c
Melissa Winstanley <mwinst@cs.washington.edu>
2012-05-04 12:41:59 -0700

    Precompiled 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,7 @@ ColdChain::Application.configure do
 
   # Generate digests for assets URLs
   config.assets.digest = true
-
+  config.assets.precompile += %w( *.css *.js )
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
 
